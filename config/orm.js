d78a6d49326fe6427d0e827d23396be5e52a76fd
d-car <danielscarstensen@gmail.com>
2018-09-04 23:10:12 -0500

    update and delete buttons for db ids rendering

--- config/orm.js
+++ config/orm.js
@@ -64,9 +64,39 @@ var orm = {
         });
     },
 
+    //update orm method 
+    update: function(table, objColVals, condition, cb) {
+        var queryString = "UPDATE " + table;
 
-    
-}
+        queryString += " SET ";
+        queryString += objToSql(objColVals);
+        queryString += " WHERE ";
+        queryString += condition;
+
+        console.log(queryString);
+        connection.query(queryString, function(err, result) {
+        if (err) {
+            throw err;
+        }
+
+        cb(result);
+        });
+    },
+    //delete orm method
+    delete: function(table, condition, cb) {
+        var queryString = "DELETE FROM " + table;
+        queryString += " WHERE ";
+        queryString += condition;
+
+        connection.query(queryString, function(err, result) {
+        if (err) {
+            throw err;
+        }
+
+        cb(result);
+        });
+    }   
+};
 
 //Export the orm object for the model (cat.js).
 module.exports = orm;
