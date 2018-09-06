5c42ec7e7990902024c044c192851cb6a69c6250
unknown <danie@Dan-Laptop>
2018-09-06 13:22:34 -0500

    create burger/post request functional with db

--- public/assets/js/burgersPublic.js
+++ public/assets/js/burgersPublic.js
@@ -1,45 +1,50 @@
 $(function() {
-    $(".btn-success").on("click", function(event) {
-      var id = $(this).data("id");
-      var newDevoured = $(this).data("newdevoured");
-  
-      var newDevouredState = {
-        devoured: true
-      };
-  
-      // Send the PUT request.
-      $.ajax("/api/burgers/" + id, {
-        type: "PUT",
-        data: newDevouredState
-      }).then(
-        function() {
-          console.log("changed burger to", newDevoured);
-          // Reload the page to get the updated list
-          // location.reload();
-        }
-      );
-    });
-  
+  $(".btn-success").on("click", function(event) {
+      console.log("clicked BT buton");
+    var id = $(this).data("id");
+    var newDevoured = $(this).data("newdevoured");
+
+    var newDevouredState = {
+      devoured: true
+    };
+
+    // Send the PUT request.
+    $.ajax("/api/burgers/" + id, {
+      type: "PUT",
+      data: newDevouredState
+    }).then(
+      function() {
+        console.log("changed burger to", newDevoured);
+        // Reload the page to get the updated list
+        location.reload();
+      }
+    );
+  });
+});
+$(function() {
+  $(".btn-primary").on("click", function(event) {
+    
     $(".create-form").on("submit", function(event) {
       // Make sure to preventDefault on a submit event.
       event.preventDefault();
   
-      var newCat = {
-        name: $("#ca").val().trim(),
-        sleepy: $("[name=sleepy]:checked").val().trim()
+      var newBurger = {
+        burger_name: $("#bName").val().trim(),
+        devoured: false
       };
   
       // Send the POST request.
-      $.ajax("/api/burger/", {
+      $.ajax("/api/burgers", {
         type: "POST",
-        data: newCat
+        data: newBurger
       }).then(
         function() {
-          console.log("created new cat");
+          console.log("created new burger");
           // Reload the page to get the updated list
           location.reload();
         }
       );
     });
 
-  });
+  })
+});
